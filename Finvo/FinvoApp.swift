53fc5c4aa12cc9c6c8642e4f6e9402fc68066b22
uaburak <design.burakkoc@gmail.com>
2025-11-17 20:50:51 +0300

    Initial Commit

---
+++ Finvo/FinvoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinvoApp.swift
+//  Finvo
+//
+//  Created by Burak KOÇ on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinvoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
