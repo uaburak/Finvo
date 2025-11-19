e28c6fa3298bf9b2964a514d5703d49bf44619c3
uaburak <design.burakkoc@gmail.com>
2025-11-19 10:10:04 +0300

    Initial Commit

---
+++ Deneme/DenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenemeApp.swift
+//  Deneme
+//
+//  Created by Burak KOÇ on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
